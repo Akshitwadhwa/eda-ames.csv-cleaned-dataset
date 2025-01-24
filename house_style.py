72214c02546d2994ea86fbf3a89931881e16e338
Akshitwadhwa <akshitwadhwa06@gmail.com>
2025-01-25 00:03:30 +0530

    explaning the data

---
+++ house_style.py
@@ -0,0 +1,16 @@
+House Style
+1.5Fin     314
+1.5Unf      19
+1Story    1481
+2.5Fin       8
+2.5Unf      24
+2Story     873
+SFoyer      83
+SLvl       128
+
+There are 8 types of house data types given to us.
+Below is the explanation of what each of the given data 
+5FIN-- 5 story and which is finished
+5unf-- 5 story and which is unfinished
+1Story-- 1 story building
+
